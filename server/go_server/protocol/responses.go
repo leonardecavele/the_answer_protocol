12e0783d68a0d1d628af01c94e84f25f43e072e0
leonardecavele <leonard.decavele@gmail.com>
2026-06-16 18:54:41 +0200

    wip

--- server/go_server/protocol/responses.go
+++ server/go_server/protocol/responses.go
@@ -5,8 +5,8 @@ import (
 	"strconv"
 )
 
+// Connection
 var (
-	// Connection.
 	ResponseHello     = "OK hello proto=" + strconv.Itoa(config.ProtocolVersion)
 	ResponseConnected = "OK connected"
 	ResponseBye       = "OK bye"
