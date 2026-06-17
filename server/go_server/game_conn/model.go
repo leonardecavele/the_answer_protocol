781ccd0222efb955048b4bf18e8cc31ccd438ab8
leonardecavele <leonard.decavele@gmail.com>
2026-06-17 12:12:50 +0200

    wip

--- server/go_server/game_conn/model.go
+++ server/go_server/game_conn/model.go
@@ -12,3 +12,13 @@ type CommandFromGameServer struct {
 	ErrorCode int    `json:"error_code"`
 	Data      string `json:"data"`
 }
+
+type QuestionToGameServer struct {
+	Question string `json:"question"`
+	Data     string `json:"data"`
+}
+
+type AnswerFromGameServer struct {
+	Question string `json:"question"`
+	Data     string `json:"data"`
+}
