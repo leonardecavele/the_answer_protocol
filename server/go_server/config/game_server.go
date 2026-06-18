19cf4da663cc7d15723245dd0dbcb847288377b5
leonardecavele <leonard.decavele@gmail.com>
2026-06-18 12:56:45 +0200

    edit: update game server config defaults and adjust command timeout

--- server/go_server/config/game_server.go
+++ server/go_server/config/game_server.go
@@ -3,11 +3,11 @@ package config
 import "time"
 
 const (
-	GameServerIP   = "10.11.12.2"
+	GameServerIP   = "localhost"
 	GameServerPort = 38801
 
 	GameConnectionRetryDelay = 5
-	GameServerCommandTimeout = 5 * time.Second
+	GameServerCommandTimeout = 3 * time.Second
 
 	GameConfirmationMessage = "Duly noted."
 )
