c37dabfaa83af7322ef2a4ca745b56119e357191
leonardecavele <leonard.decavele@gmail.com>
2026-09-15 14:28:27 +0200

    edit: refactor

--- server/go_server/helper/network.go
+++ server/go_server/helper/network.go
@@ -1,9 +1,6 @@
 package helper
 
 import (
-	"bufio"
-	"errors"
-	serverError "go_server/error"
 	"net"
 	"strings"
 )
@@ -18,33 +15,6 @@ func NormalizeIP(value string) string {
 		return ""
 	}
 	return ip.String()
-}
-
-func ReadStringWithLimit(reader *bufio.Reader, delimiter byte, maxSize int) (string, error) {
-	fragment, err := reader.ReadSlice(delimiter)
-	if len(fragment) > maxSize {
-		return "", serverError.ErrReadStringTooLong
-	}
-	if !errors.Is(err, bufio.ErrBufferFull) {
-		return string(fragment), err
-	}
-
-	message := append([]byte(nil), fragment...)
-
-	for {
-		fragment, err = reader.ReadSlice(delimiter)
-		if len(fragment) > maxSize-len(message) {
-			return string(message), serverError.ErrReadStringTooLong
-		}
-		message = append(message, fragment...)
-
-		if err == nil {
-			return string(message), nil
-		}
-		if !errors.Is(err, bufio.ErrBufferFull) {
-			return string(message), err
-		}
-	}
 }
 
 func GetServerIP() string {
