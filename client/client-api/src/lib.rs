7ae873539616ef788cf13784e0fbadd14a1d5971
parad0xe <parad0xe@protonmail.com>
2026-09-16 09:56:19 +0200

    fix: cargo fmt

--- client/client-api/src/lib.rs
+++ client/client-api/src/lib.rs
@@ -12,10 +12,10 @@ pub use protocol::response::{Opcode, ServerResponse};
 
 pub mod events {
     pub use crate::client::event::{
-        ChatEvent, ChatMessage, CounterAttackData, DeathData, FightEndData, FightEvent,
-        FightResultData, FightStartData, GameServerEvent, GroupEvent, ItemEvent, KillData,
-        QuestCompleteData, QuestEvent, QuestStepData, RoomEvent, ServerEvent, SessionEvent,
-        SpawnData,
+        ChatEvent, ChatMessage, CounterAttackData, DeathData, FightEndData, FightEndPlayerData,
+        FightEvent, FightResultData, FightStartData, GameServerEvent, GroupEvent, ItemEvent,
+        KillData, QuestCompleteData, QuestEvent, QuestStepData, RoomEvent, ServerEvent,
+        SessionEvent, SpawnData,
     };
 }
 
